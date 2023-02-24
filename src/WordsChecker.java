ebfd10308d7413fe97c300200fd66565008ff3c5
Anastasiya <lazycat73332@gmail.com>
2023-02-24 14:44:32 +0300

    Изменены методы: WordsChecker - в тексте теперь не учитывается регистр, деление на слова происходит с помощью text.split("\\P{IsAlphabetic}+" hasWord - теперь не учитывает регистр передаваемого слова

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -3,9 +3,11 @@ public class WordsChecker {
     private Set<String> set;
 
     public WordsChecker(String text) {
-        set=new HashSet<>(List.of(text.split(" ")));
+        text.toLowerCase();
+        set=new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
     }
     public boolean hasWord(String word){
+        word.toLowerCase();
         if (set.contains(word)) return true;
         return false;
     }
